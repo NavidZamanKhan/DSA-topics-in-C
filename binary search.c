ed97aa9b28cee226e9b55db01d077eb729bdfe66
Navid444 <navid166418@gmail.com>
2024-11-03 17:54:17 +0600

    added few more algorithms

---
+++ binary search.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key)
+            return mid;
+        if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 10;
+    int result = binarySearch(arr, 0, n - 1, key);
+    (result == -1) ? printf("Element not present\n")
+                   : printf("Element found at index %d\n", result);
+    return 0;
+}
